1e251ed2b68c2b3cd44fd1970d2788e17e697dda
shivam7703 <shivamg7703@gmail.com>
2026-06-13 11:03:03 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   distDir: 'out',
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    esmExternals: false,
   },
 };
 
