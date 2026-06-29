1cd74ca53e823ef04cb7668a7ecada6a3fe543a5
shivam7703 <shivamg7703@gmail.com>
2026-06-29 15:33:54 +0530

    lll

--- src/components/visa/side.tsx
+++ src/components/visa/side.tsx
@@ -9,32 +9,32 @@ const data = [
   {
     id: 1,
     title: "Canada PR Visa",
-    href: "/visa/canada-pr-visa",
+    href: "/visa/canada-pr-visa/",
   },
   {
     id: 2,
     title: "Australia PR Visa",
-    href: "/visa/australia-pr-visa",
+    href: "/visa/australia-pr-visa/",
   },
   {
     id: 3,
     title: "Dubai Work Visa",
-    href: "/visa/dubai-work-visa",
+    href: "/visa/dubai-work-visa/",
   },
   {
     id: 4,
     title: "Qatar Tourist Visa",
-    href: "/visa/qatar-tourist-visa",
+    href: "/visa/qatar-tourist-visa/",
   },
   {
     id: 5,
     title: "Croatia Work Visa",
-    href: "/visa/croatia-work-visa",
+    href: "/visa/croatia-work-visa/",
   },
   {
     id: 6,
     title: "Luxembourg Work Visa",
-    href: "/visa/luxembourg-work-visa",
+    href: "/visa/luxembourg-work-visa/",
   },
 ];
 function VisaAside() {
@@ -86,7 +86,7 @@ function VisaAside() {
 
             <Link
               title="Best immigration consultants in Delhi"
-              href={"/contact-us"}
+              href={"/contact-us/"}
               className="max-w-fit"
             >
               <div className="flex  gap-2 p-2 max-w-fit rounded-3xl text-black hover:text-color1 font-medium  group/btn relative bg-white overflow-hidden items-center flex-row h-full">
